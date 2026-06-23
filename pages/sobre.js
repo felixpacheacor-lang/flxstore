6a5ffb8425e65aeeb0381dd374ecbaf8087c5ce7
felixpacheacor-lang <felixpacheacor@gmail.com>
2026-06-23 11:17:47 -0300

    Create sobre.js

---
+++ pages/sobre.js
@@ -0,0 +1,12 @@
+export default function Sobre() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Sobre a FLX Store</h1>
+
+      <p>
+        A FLX Store reúne ofertas de esportes, academia,
+        eletrônicos, celulares, acessórios e eletrodomésticos.
+      </p>
+    </div>
+  );
+}
