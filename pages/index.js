ad6054904a96af8026287099558c155be93cdc37
felixpacheacor-lang <felixpacheacor@gmail.com>
2026-06-23 08:29:10 -0300

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: "Arial", padding: 20 }}>
-      <h1>FLX Store</h1>
-      <p>Sua loja dropshipping está no ar 🚀</p>
-    </div>
-  );
-}
