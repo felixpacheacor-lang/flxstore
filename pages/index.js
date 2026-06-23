0a0262965633c3bb5589284d19e6242401e06862
felixpacheacor-lang <felixpacheacor@gmail.com>
2026-06-23 08:42:50 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: 20 }}>
+      <h1>FLX Store</h1>
+      <p>Sua loja dropshipping está no ar 🚀</p>
+    </div>
+  );
+}
