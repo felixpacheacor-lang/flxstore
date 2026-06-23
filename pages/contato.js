c617824363e9e0939850bfda92747aaf43b05f0b
felixpacheacor-lang <felixpacheacor@gmail.com>
2026-06-23 11:16:42 -0300

    Create contato.js

---
+++ pages/contato.js
@@ -0,0 +1,11 @@
+export default function Contato() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Contato</h1>
+
+      <p>WhatsApp: (11) 99121-7277</p>
+
+      <p>E-mail: felixpacheacor@gmail.com</p>
+    </div>
+  );
+}
