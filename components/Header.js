d067ae3a04a6a4a3915e7155e0c0c914e6877653
felixpacheacor-lang <felixpacheacor@gmail.com>
2026-06-23 17:37:35 -0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,1 +1,17 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
+export default function Home() {
+  return (
+    <>
+      <Header />
+
+      {/* Banner */}
+      {/* Categorias */}
+      {/* Produtos em destaque */}
+      {/* Ofertas */}
+
+      <Footer />
+    </>
+  );
+}
