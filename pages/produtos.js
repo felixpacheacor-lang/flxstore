dd73ea05e416d82d283ddf6884ca458c03645f3c
felixpacheacor-lang <felixpacheacor@gmail.com>
2026-06-23 11:15:24 -0300

    Create produtos.js

---
+++ pages/produtos.js
@@ -0,0 +1,9 @@
+export default function Produtos() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Produtos FLX Store</h1>
+
+      <p>Em breve produtos reais com links de afiliado.</p>
+    </div>
+  );
+}
