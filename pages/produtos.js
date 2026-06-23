83b7e55f05ab27fb7c1abe4ce5c05e97b0593d30
felixpacheacor-lang <felixpacheacor@gmail.com>
2026-06-23 18:02:58 -0300

    Delete pages/produtos.js

--- pages/produtos.js
+++
@@ -1,9 +0,0 @@
-export default function Produtos() {
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Produtos FLX Store</h1>
-
-      <p>Em breve produtos reais com links de afiliado.</p>
-    </div>
-  );
-}
