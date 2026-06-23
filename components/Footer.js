7e4a11d6f376acdf01f484d6d9f27f6dde9a4d85
felixpacheacor-lang <felixpacheacor@gmail.com>
2026-06-23 17:40:34 -0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,1 +1,21 @@
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        background: "#0057D9",
+        color: "#fff",
+        padding: "30px 20px",
+        textAlign: "center",
+        marginTop: "50px",
+      }}
+    >
+      <h3>FLX Store</h3>
 
+      <p>
+        Sua loja online de tecnologia, eletrônicos, moda, casa e muito mais.
+      </p>
+
+      <p>© 2026 FLX Store - Todos os direitos reservados.</p>
+    </footer>
+  );
+}
