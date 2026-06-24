5f64a67ca20de00b8de9f31aedc0e3d1e5be6978
felixpacheacor-lang <felixpacheacor@gmail.com>
2026-06-24 11:19:20 -0300

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header style={{
+      background: "#111827",
+      color: "#fff",
+      padding: "20px",
+      textAlign: "center",
+      fontSize: "24px",
+      fontWeight: "bold"
+    }}>
+      FLX Store
+    </header>
+  );
+}
